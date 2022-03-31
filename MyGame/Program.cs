c3f078eaa7c980d5403dc009a8400baaff6d0e3d
Micael Teixeira <a22103816@alunos.ulht.pt>
2022-03-31 11:48:38 +0200

    Create enemy array, ask their name, show every enemy

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,27 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Vars
+            int nEnemies;
+            string enemyName;
+
+            //Ask user enemy number
+            Console.Write("Number of enemies: ");
+            nEnemies = Convert.ToInt32(Console.ReadLine());
+
+            Enemy[] allEnemies = new Enemy[nEnemies];
+
+            for(int i=0; i<nEnemies; i++)
+            {
+                Console.Write($"Enemy {i+1} name: ");
+                enemyName = Console.ReadLine();
+                allEnemies[i] = new Enemy(enemyName);
+            }
+
+            for(int i=0; i<allEnemies.Length; i++)
+            {
+                Console.WriteLine(allEnemies[i].GetName());
+            }            
         }
     }
 }
