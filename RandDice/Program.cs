dbd4e5b6f7bb00d97c766dc450e1bde2ae55dade
Drackout <mikatx2@hotmail.com>
2025-03-27 01:14:28 +0000

    Finish exercice 5

--- RandDice/Program.cs
+++ RandDice/Program.cs
@@ -6,7 +6,16 @@ namespace RandDice
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Random r1 = new Random(int.Parse(args[1]));
+
+            int sumRand = 0;
+
+            for (int i = 0; i < int.Parse(args[0]); i++)
+            {  
+                sumRand += r1.Next(1, 7);
+            }
+
+            Console.WriteLine(sumRand);
         }
     }
 }
